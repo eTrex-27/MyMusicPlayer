e14a5697a3752cb5697ab319fa1dca4888881de6
eTrex-27 <e.chernyavtsev@gmail.com>
2022-07-07 22:39:16 +0700

    Addition files with click button and drag'n'drop

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Popups;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace MyMusicPlayer
 {
@@ -45,6 +50,88 @@ namespace MyMusicPlayer
             LabelOR.Foreground = pageInfo.LabelOrColor;
             LabelDragAndDrop.Text = pageInfo.LabelDragAndDropText;
             LabelDragAndDrop.Foreground = pageInfo.LabelDragAndDropColor;
+        }
+
+        private async void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<StorageFile> files = null;
+
+            try
+            {
+                FileOpenPicker openPicker = new FileOpenPicker();
+                openPicker.ViewMode = PickerViewMode.List;
+                openPicker.SuggestedStartLocation = PickerLocationId.MusicLibrary;
+                openPicker.FileTypeFilter.Add(".mp3");
+                openPicker.FileTypeFilter.Add(".wav");
+                var filesList = await openPicker.PickMultipleFilesAsync();
+
+                files = filesList.ToList();
+            }
+            catch
+            {
+                var dialog = new MessageDialog("Не удалось загрузить файлы, попробуйте ещё раз");
+                await dialog.ShowAsync();
+            }
+
+            if (files != null)
+            {
+                try
+                {
+                    Frame.Navigate(typeof(MusicPage), files, new SuppressNavigationTransitionInfo());
+                }
+                catch
+                {
+                    var dialog = new MessageDialog("Не удалось открыть страницу с плеером, попробуйте снова выбрать файлы");
+                    await dialog.ShowAsync();
+                }
+            }
+        }
+
+        private void Grid_DragOverCustomized(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Отпустите, чтобы добавить";
+            e.DragUIOverride.IsCaptionVisible = true;
+            e.DragUIOverride.IsContentVisible = true;
+            e.DragUIOverride.IsGlyphVisible = true;
+            this.Opacity = 0.35;
+        }
+
+        private void Grid_DragLeave(object sender, DragEventArgs e)
+        {
+            this.Opacity = 1.0;
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            List<StorageFile> listMusic = new List<StorageFile>();
+
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                if (items.Count > 0)
+                {
+                    foreach (var item in items)
+                    {
+                        var storageFile = item as StorageFile;
+
+                        if (storageFile.FileType == ".mp3" || storageFile.FileType == ".wav")
+                        {
+                            listMusic.Add(storageFile);
+                        }
+                    }
+
+                    try
+                    {
+                        Frame.Navigate(typeof(MusicPage), listMusic, new SuppressNavigationTransitionInfo());
+                    }
+                    catch
+                    {
+                        var dialog = new MessageDialog("Не удалось открыть страницу с плеером, попробуйте снова выбрать файл");
+                        await dialog.ShowAsync();
+                    }
+                }
+            }
         }
     }
 }
---
+++ MusicPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MyMusicPlayer
+{
+    public sealed partial class MusicPage : Page
+    {
+        public MusicPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var files = (List<StorageFile>)e.Parameter;
+
+            var musicList = new List<string>();
+
+            foreach (StorageFile file in files)
+            {
+                musicList.Add(file.Name);
+            }
+
+            listMusic.ItemsSource = musicList;
+        }
+    }
+}
