baa13cbdedf8cfe945521ef24f30b4e42317e042
eTrex-27 <e.chernyavtsev@gmail.com>
2022-07-08 18:11:08 +0700

    Create layout for Music Page

--- MusicPage.xaml.cs
+++ MusicPage.xaml.cs
@@ -12,15 +12,20 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 namespace MyMusicPlayer
 {
     public sealed partial class MusicPage : Page
     {
+        BitmapImage bitmapPlay = new BitmapImage(new Uri("ms-appx:///Assets/play.png", UriKind.Absolute));
+        BitmapImage bitmapPause = new BitmapImage(new Uri("ms-appx:///Assets/pause.png", UriKind.Absolute));
         public MusicPage()
         {
             this.InitializeComponent();
+
+            PlayImage.Source = bitmapPlay;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -35,6 +40,43 @@ namespace MyMusicPlayer
             }
 
             listMusic.ItemsSource = musicList;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayImage.Source == bitmapPlay)
+            {
+                PlayImage.Source = bitmapPause;
+            }
+            else
+            {
+                PlayImage.Source = bitmapPlay;
+            }
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Repeat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shuffle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Volume_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
